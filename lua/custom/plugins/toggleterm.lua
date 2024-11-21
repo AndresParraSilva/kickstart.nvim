37e02be4809c048e8f874d012218f93115418f4c
andresparraglobant <andres.parra@globant.com>
2024-11-21 09:50:55 -0300

    toggleterm

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -40,17 +40,17 @@ return {
       lazygit:toggle()
     end
 
-    -- local node = Terminal:new { cmd = 'node', hidden = true }
-    --
-    -- function _NODE_TOGGLE()
-    --   node:toggle()
-    -- end
-    --
-    -- local ncdu = Terminal:new { cmd = 'ncdu', hidden = true }
-    --
-    -- function _NCDU_TOGGLE()
-    --   ncdu:toggle()
-    -- end
+    local node = Terminal:new { cmd = 'node', hidden = true }
+
+    function _NODE_TOGGLE()
+      node:toggle()
+    end
+
+    local ncdu = Terminal:new { cmd = 'ncdu', hidden = true }
+
+    function _NCDU_TOGGLE()
+      ncdu:toggle()
+    end
 
     local htop = Terminal:new { cmd = 'htop', hidden = true }
 
